5703b73c4f898bc32432de6f8366c5eba92e1471
bruno-gazola <159180432+bruno-gazola@users.noreply.github.com>
2024-11-12 08:17:40 -0300

    Delete ATIVIIDADE12 directory

--- ATIVIIDADE12/saraiva/src/main/java/com/senaidev/saraiva/repositories/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senaidev.saraiva.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senaidev.saraiva.entities.Livro;
-
-public interface LivroRepository extends JpaRepository<Livro, Long>{
-
-}
